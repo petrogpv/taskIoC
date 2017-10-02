eefe5e9d914206e25d576ce2edefecd9e58f890b
petrogpv <gorpetval1982>
2017-10-02 16:53:44 +0300

    1

--- src/main/java/ua/rd/repository/InMemUserRepository.java
+++ src/main/java/ua/rd/repository/InMemUserRepository.java
@@ -11,7 +11,7 @@ public class InMemUserRepository implements UserRepository {
     private Map<Long,User> userRepo = new HashMap<>();
 
     public InMemUserRepository(){}
-    
+
     @Override
     public User save(User user) {
         if(user.getId()==null) {
